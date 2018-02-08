ba29d8fa9b3cf4a9070f6f277a61e4598daac889
Adalia <haihui_blessed@163.com>
2018-02-08 09:36:47 +0800

    multiple database

--- MyDjangoProject/testdb.py
+++ MyDjangoProject/testdb.py
@@ -2,8 +2,8 @@
 
 from django.http import HttpResponse
 from model.models import *
-from app01.models import Users
-from app02.models import User
+from app01.models import User
+from app02.models import UserInfo
 # 数据库操作
 def testdb(request):
     Test(name='hahr33333ha').save()
@@ -11,12 +11,12 @@ def testdb(request):
 
 
 def app01(request):
-    Users(username='app013333', password='app01pass', age=20).save()
+    User(username='app013333', password='app01pass', age=20).save()
     return HttpResponse("<p>app01数据添加成功！</p>")
 
 
 def app02(request):
-    User(name='app02', passwd='app02222pass').save()
+    UserInfo(name='app02', passwd='app02222pass').save()
     return HttpResponse("<p>app02数据添加成功！</p>")
 
 
